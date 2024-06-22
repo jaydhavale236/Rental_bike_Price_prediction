a8b4032297de05fdeb49363e4bc3aa0512e90ada
JAYDIP MOHAN DHAVALE <68918708+jaydhavale236@users.noreply.github.com>
2024-06-22 23:42:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,6 +44,6 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)})
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port= 10000)
     
     

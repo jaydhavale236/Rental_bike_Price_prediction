1fb435bda8ab4004e35173e5e8127a879c84cb67
JAYDIP MOHAN DHAVALE <68918708+jaydhavale236@users.noreply.github.com>
2024-06-23 00:15:47 +0530

    Update form.py

--- form.py
+++ form.py
@@ -33,6 +33,3 @@ def index():
         flash(f'Hello, {form.name.data}!', 'success')
         return redirect(url_for('index'))
     return render_template('index.html', form=form)
-
-if __name__ == '__main__':
-    app.run(debug=True port=10000)
